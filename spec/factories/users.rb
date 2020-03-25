24c8c1705a0d4c8f88c546a3a00a914b335a0172
ゆーひ <ahiru@ahirunoMacBook-Pro.local>
2020-03-25 12:05:49 +0900

    rspecのいらないコードを消去

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -6,32 +6,4 @@ FactoryBot.define do
     password_confirmation { 'password' }
     user_private { false }
   end
-  factory :user1, class: User do
-    name { 'kingmasa' }
-    email { 'kingmasa@gmail.com' }
-    password { 'a' }
-    password_confirmation { 'a' }
-    user_private { false }
-  end
-  factory :user2, class: User do
-    name { 'kingyuhi' }
-    email { 'kingyuhi@gmail.com' }
-    password { 'a' }
-    password_confirmation { 'a' }
-    user_private { false }
-  end
-  factory :follower, class: User do
-    sequence(:name) { |n| "kinggod#{n}" }
-    sequence(:email) { |n| "factory#{n}@example.com" }
-    password { 'password' }
-    password_confirmation { 'password' }
-    user_private { false }
-  end
-  factory :followed, class: User do
-    sequence(:name) { |n| "kingkitaken#{n}" }
-    sequence(:email) { |n| "factoryy#{n}@example.com" }
-    password { 'password' }
-    password_confirmation { 'password' }
-    user_private { false }
-  end
 end
